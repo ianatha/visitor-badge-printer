04a3777942414a9f260836a0ddb712976e37ac7d
Ian W. Atha <ian@atha.io>
2013-04-13 13:14:42 -0700

    rename to moat

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "frontdesk2"
+  val appName         = "moat"
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
