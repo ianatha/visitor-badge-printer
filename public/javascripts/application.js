9632743b5aa40d0534c9c81dab651c011c031800
Ian W. Atha <ian@atha.io>
2013-04-15 19:49:28 -0700

    plug the js into the API and make the 'present' page work

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,23 +1,31 @@
-(function () {
-
-}).call(this), function () {
-}.call(this), function () {
-    var e;
-    e = jQuery, e.fn.vAlign = function (t) {
+$(function() {
+    $.fn.vAlign = function (t) {
         return this.each(function (n) {
             var r, i, s;
-            return r = e(this).height(), s = e(this).parent().height(), i = Math.ceil((s - r) * t), e(this).css("padding-top", i)
+            return r = $(this).height(), s = $(this).parent().height(), i = Math.ceil((s - r) * t), $(this).css("padding-top", i)
         })
-    }, e(document).ready(function () {
-        e("#splashButtons").vAlign(.43), e("#nda-agree").live("click tap", function (t) {
-            return e(".nda-accepted").value(1)
-        }), e(document).bind("mobileinit", function () {
-            return e.extend(e.mobile, {metaViewportContent: "width=device-width, height=device-height, minimum-scale=1, maximum-scale=1"})
-        }), e(".deleteButton").click(function () {
-            var t, n;
-            return t = e(event.target).closest("a"), n = t.attr("data-person-id"), e.get("/signout", {first_name: t.attr("data-first"), last_name: t.attr("data-last")}, function (e) {
-                return window.location.reload()
-            }), !1
+    };
+
+    $(document).ready(function () {
+        $("#splashButtons").vAlign(.43);
+
+        $("#nda-agree").live("click tap", function(t) {
+            return $(".nda-accepted").value(1);
+        });
+
+        $(document).bind("mobileinit", function () {
+            return $.extend($.mobile, {
+                metaViewportContent: "width=device-width, height=device-height, minimum-scale=1, maximum-scale=1"
+            });
+        });
+
+        $(".deleteButton").click(function (e) {
+            console.log(e);
+            console.log(event)
+            var id = $(event.target).closest("a").attr("data-id");
+            $.post("/api/1/signout/" + id, {}, function(e) {
+                return window.location.reload();
+            });
         })
     })
-}.call(this);
+});
