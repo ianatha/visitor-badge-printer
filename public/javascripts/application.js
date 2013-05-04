ef3aa64f7df8fe7b4264c6c621d980abf483c6ad
Ian Atha <ian@atha.io>
2013-05-03 21:58:07 -0700

    hacky support for capturing id scans

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,22 @@
+var buf = [];
+var last_timestamp = 0;
+var maybe_char = false;
+var enabled = true;
+
+function simulateKeyPress(character) {
+  jQuery.event.trigger({ type : 'keypress', which : character.charCodeAt(0) });
+}
+
+function fastInput(s) {
+    console.log(s);
+}
+
+function keycodesToString(k) {
+    return $.map(k, function (val, i) {
+        return String.fromCharCode(val);
+    }).join("");
+}
+
 $(function() {
     $.fn.vAlign = function (t) {
         return this.each(function (n) {
@@ -7,6 +26,49 @@ $(function() {
     };
 
     $(document).ready(function () {
+
+        $("body").keypress(function (e) {
+            if (!enabled) {
+                return;
+            }
+
+            var key = e.which;
+            var when = e.timeStamp;
+
+            var delta = when - last_timestamp;
+            if (delta <= 40) {
+                event.preventDefault();
+                if (maybe_char != false) {
+                    buf.push(maybe_char);
+                    maybe_char = false;
+                }
+            } else {
+                if (maybe_char != false) {
+                    console.log("not maybe but can't type it");
+                    maybe_char = false;
+                }
+            }
+
+
+            if (key != 13) {
+                buf.push(key);
+            } else {
+                var s = keycodesToString(buf);
+                buf = []
+                fastInput(s);
+            }
+
+            if (delta > 500) {
+                buf = [];
+                if (key == 37) {
+                    console.log("maybe");
+                    maybe_char = key;
+                    event.preventDefault();
+                }
+            }
+            last_timestamp = when;
+        });
+
         $("#splashButtons").vAlign(.43);
 
         $("#nda-agree").live("click tap", function(t) {
