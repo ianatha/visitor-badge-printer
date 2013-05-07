f9ba20f735214b2ebe65d90e773315ce612046e1
Ian Atha <ian@atha.io>
2013-05-07 16:08:49 -0700

    quick'n'dirty script to distribute the latest version of moat to the box named 'moat'

---
+++ dist.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# to use a different user, just run this command as USER=jdoe ./dish.sh
+
+HOST=moat
+ARTIFACT=moat-1.0-SNAPSHOT
+
+sbt dist
+
+ssh $USER@$HOST "rm -rf $ARTIFACT && rm -rf $ARTIFACT.zip"
+scp dist/$ARTIFACT.zip $USER@$HOST:.
+ssh $USER@$HOST "unzip $ARTIFACT.zip"
+
+ssh $USER@$HOST "kill \`cat $ARTIFACT-instance-1/RUNNING_PID\` && rm -rf $ARTIFACT-instance-1 && cp -R $ARTIFACT $ARTIFACT-instance-1"
+ssh $USER@$HOST "chmod +x $ARTIFACT-instance-1/start && nohup $ARTIFACT-instance-1/start -Dconfig.file=/home/$USER/application.conf -Dhttp.port=9000 > /dev/null &"
+
+ssh $USER@$HOST "kill \`cat $ARTIFACT-instance-2/RUNNING_PID\` && rm -rf $ARTIFACT-instance-2 && cp -R $ARTIFACT $ARTIFACT-instance-2"
+ssh $USER@$HOST "chmod +x $ARTIFACT-instance-2/start && nohup $ARTIFACT-instance-2/start -Dconfig.file=/home/$USER/application.conf -Dhttp.port=9001 > /dev/null &"
