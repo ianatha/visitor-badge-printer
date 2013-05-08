47f20822581d81c6f689dd799c56ed09854e053f
Ian Atha <ian@atha.io>
2013-05-08 10:10:21 -0700

    make the api compatible with the printer, maybe

--- app/controllers/API.scala
+++ app/controllers/API.scala
@@ -7,14 +7,16 @@ import play.api.mvc._
 import templates.Html
 import models._
 import models.Person
-import org.joda.time.DateTime
+import org.joda.time.{DateTimeZone, DateTime}
 import play.api.data.format.Formats.jodaDateTimeFormat
 import java.util.UUID
 
 import play.api.data._
 import play.api.data.Forms._
 import play.api.data.validation.Constraints._
-import org.joda.time.format.DateTimeFormatter
+import org.joda.time.format.{ISODateTimeFormat, DateTimeFormat, DateTimeFormatter}
+import java.sql.Timestamp
+import org.joda.time.DateTimeZone._
 
 object API extends Controller {
  def redb = Action { implicit request =>
@@ -35,36 +37,32 @@ object API extends Controller {
     Ok("")
  }
 
+  def personsToJsons(visitor: Seq[Person]) = Json.toJson(visitor.map { v =>
+    Map(
+      "id" -> v.id.toString
+      ,"name" -> v.first_name
+      ,"last" -> v.last_name
+      ,"host" -> v.host
+      ,"created_at" -> timestamp2datetime(v.created_at).toDateTime(DateTimeZone.forID("America/Los_Angeles")).toString(ISODateTimeFormat.dateTime())
+    )
+  })
+
   def present = Action {
     val visitor = AppDB.database.withSession { implicit session: scala.slick.session.Session =>
       AppDB.dal.Persons.present()
     }
 
-    Ok(Json.toJson(visitor.map { v =>
-      Map(
-        "id" -> v.id.toString
-        ,"name" -> v.first_name
-        ,"last" -> v.last_name
-        ,"host" -> v.host
-        ,"created_at" -> v.created_at.toString()
-      )
-    }))
+    Ok(personsToJsons(visitor))
   }
+
+  def timestamp2datetime(a: Timestamp): DateTime = new DateTime(a.getTime, UTC)
 
   def visitor(id: java.util.UUID) = Action {
     val visitor = AppDB.database.withSession { implicit session: scala.slick.session.Session =>
       AppDB.dal.Persons.get(id)
     }.headOption
 
-    Ok(Json.toJson(visitor.map { v =>
-      Map(
-        "id" -> v.id.toString
-        ,"name" -> v.first_name
-        ,"last" -> v.last_name
-        ,"host" -> v.host
-        ,"created_at" -> v.created_at.toString()
-      )
-    }))
+    Ok(personsToJsons(visitor.toList))
   }
 
   def last_few_events() = Action {
