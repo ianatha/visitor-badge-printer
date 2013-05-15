50270ccfc1e0d2aab25ccb08262ae3919ab795bd
Ian Atha <ian@atha.io>
2013-05-15 04:34:44 -0700

    "better" dist script.

--- dist.sh
+++ dist.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# to use a different user, just run this command as USER=jdoe ./dish.sh
-
+USER=moat
 HOST=moat
 ARTIFACT=moat-1.0-SNAPSHOT
 
@@ -12,7 +11,7 @@ scp dist/$ARTIFACT.zip $USER@$HOST:.
 ssh $USER@$HOST "unzip $ARTIFACT.zip"
 
 ssh $USER@$HOST "kill \`cat $ARTIFACT-instance-1/RUNNING_PID\` && rm -rf $ARTIFACT-instance-1 && cp -R $ARTIFACT $ARTIFACT-instance-1"
-ssh $USER@$HOST "chmod +x $ARTIFACT-instance-1/start && nohup $ARTIFACT-instance-1/start -Dconfig.file=/home/$USER/application.conf -Dhttp.port=9000 > /dev/null &"
+ssh $USER@$HOST "chmod +x $ARTIFACT-instance-1/start && nohup $ARTIFACT-instance-1/start -Dconfig.file=conf/application.conf -Dhttp.port=9000 > /dev/null &"
 
 ssh $USER@$HOST "kill \`cat $ARTIFACT-instance-2/RUNNING_PID\` && rm -rf $ARTIFACT-instance-2 && cp -R $ARTIFACT $ARTIFACT-instance-2"
-ssh $USER@$HOST "chmod +x $ARTIFACT-instance-2/start && nohup $ARTIFACT-instance-2/start -Dconfig.file=/home/$USER/application.conf -Dhttp.port=9001 > /dev/null &"
+ssh $USER@$HOST "chmod +x $ARTIFACT-instance-2/start && nohup $ARTIFACT-instance-2/start -Dconfig.file=conf/application.conf -Dhttp.port=9001 > /dev/null &"
